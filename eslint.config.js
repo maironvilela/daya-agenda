82833a65e41131dc0c613271416cbd86d241c5d1
Mairon <maironvilela@gmail.com>
2025-03-05 19:31:11 -0300

    chore: project setup

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import path from "path";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "dist/**",
+      "**/*.config.js",
+      "**/*.config.mjs"
+    ]
+  },
+  {
+    files: ["src/**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    plugins: {
+      "@stylistic": stylistic,
+      quotes: ["error", "single"], // Mudar para single quotes
+
+    },
+
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: path.dirname(import.meta.url).replace(/^file:\/\//, '')
+      }
+    },
+
+    rules: {
+      "quotes": ["error", "single", { "avoidEscape": true }],
+       "no-unused-vars": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "semi": ['error', 'always']
+
+    },
+  },
+  {
+    files: ["**/*.spec.ts"],
+   
+  },
+  
+);
